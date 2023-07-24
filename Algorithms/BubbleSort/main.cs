20c24c3a1393b8ee7db3ab598a5bf2c04eb6119f
yasin-yumrutas <yasinyumrutas0@gmail.com>
2023-07-24 13:17:02 +0300

    algorithms
    with csharp SOLID rules

---
+++ Algorithms/BubbleSort/main.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+
+interface SortingStrategy
+{
+    void Sort(int[] arr);
+}
+
+
+class BubbleSort : SortingStrategy
+{
+    public void Sort(int[] arr)
+    {
+        int n = arr.Length;
+        bool swapped;
+        for (int i = 0; i < n - 1; i++)
+        {
+            swapped = false;
+            for (int j = 0; j < n - i - 1; j++)
+            {
+                if (arr[j] > arr[j + 1])
+                {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+
+            if (!swapped)
+                break;
+        }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] arr = { 64, 34, 25, 12, 22, 11, 90 };
+        SortingStrategy sorter = new BubbleSort();
+        sorter.Sort(arr);
+
+        Console.WriteLine("Sorted array:");
+        foreach (int num in arr)
+        {
+            Console.Write(num + " ");
+        }
+    }
+}
+
+
